7893a8c99d5966fa1685c709125eb2d9748770cb
Rohan Dutt <duttrohan0302@gmail.com>
2021-09-14 23:22:34 +0530

    mosaic creation

--- index.js
+++ index.js
@@ -2,27 +2,56 @@ const express = require('express')
 const { spawn } = require('child_process')
 const app = express()
 const port = 3000
+const path=require("path");
+const ejs=require("ejs");
+const multer = require('multer')
 
-app.get('/', (req, res) => {
-  let dataToSend
-  let largeDataSet = []
-  // spawn new child process to call the python script
-  const python = spawn('python', ['script3.py'])
-
-  // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...')
-    //dataToSend =  data;
-    largeDataSet.push(data)
-  })
-
-  // in close event we are sure that stream is from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`)
-    // send data to browser
-    res.send(largeDataSet.join(''))
-  })
+const upload = multer({
+  dest: 'data',
+  limits: {
+    fileSize: 1000000,
+  },
+  
+  fileFilter(req, file, cb) {
+  
+  if (!file.originalname.match(/\.(png|jpg|jpeg)$/)){
+    cb(new Error('Please upload an image.'))
+  }
+    cb(undefined, true)
+  }
 })
+  
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+  if(req.file){
+
+    console.log("File name",req.body.filename)
+    const python = spawn('python', ['creator.py',`data/`+req.file.filename,'data/Trees/','100','100','public/Mosaic.jpeg'])
+
+    python.stdout.on('data', function (data) {
+      console.log('Pipe data from python script ...')
+      console.log(data.toString())
+  
+    })
+
+    python.stdout.on('close', function (code) {
+      console.log('Closed with code ',code)
+      res.render('pages/image',{data: "Mosaic.jpeg"});
+    })
+  }else{
+    res.status(400).json({msg:"Img Not Received"})
+  }
+})
+
+app.get('/',(req,res)=>{
+  console.log("Home Route")
+  res.render('pages/index');
+})
+
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`)
